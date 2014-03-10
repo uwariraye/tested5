33f00c17a10d057a696492a9bef6a6a5ce92302d
fidos <fidos@fidos-VirtualBox.(none)>
2014-03-10 18:23:43 +0200

    first commit

---
+++ list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+argument=$#;
+argument=$@;
+echo;i=1;
+for argument in "$@"
+do
+echo "argument $i=$argument";
+let "i+1"
+done
+echo "$argument">>names.txt;
+echo "$# names have been added"
